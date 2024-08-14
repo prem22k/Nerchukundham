c31f71023d17946e617d2733b7b36cda38272d12
Prem Sai K <premsai224k@gmail.com>
2024-08-14 16:57:34 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const icon=document.querySelector("#icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "sun.png";
+    }else{
+        icon.src = "moon.png";
+    }
+}
